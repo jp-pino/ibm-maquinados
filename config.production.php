bb11292dfc964f64f7825d533935b9d70bfd9fe2
Juan Pablo Pino Bravo <jp.pinob@utexas.edu>
2020-01-12 12:30:54 -0600

    Update production config

--- config.production.php
+++ config.production.php
@@ -2,4 +2,9 @@
 
 return [
     'production' => true,
+    'baseUrl' => 'https://jp-pino.github.io/ibm-maquinados',
+    'collections' => [],
+    'isActive' => function ($page, $path) {
+        return ends_with(trimPath($page->getPath()), trimPath($path));
+    },
 ];
