02e6af133bad01099ca1d04a4815e62977f0ee9a
Juan Pablo Pino Bravo <jp.pinob@utexas.edu>
2020-01-23 19:30:48 -0600

    Fix netlify

--- config.netlify.php
+++ config.netlify.php
@@ -2,7 +2,7 @@
 
 return [
     'production' => true,
-    'baseUrl' => '/',
+    'baseUrl' => 'https://goofy-goldstine-45d1a2.netlify.com/',
     'collections' => [],
     'isActive' => function ($page, $path) {
         return ends_with(trimPath($page->getPath()), trimPath($path));
